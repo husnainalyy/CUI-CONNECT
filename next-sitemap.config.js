10065463386d95021413381e52d75f4bf64a4637
Husnain Ali <alyhusnaiin@gmail.com>
2024-08-25 14:13:04 +0500

    comsats connect

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+    siteUrl: 'https://cui-connect.vercel.app', // Replace with your Vercel URL
+    generateRobotsTxt: true, // (optional) Generate robots.txt file
+    // ...other options
+};
