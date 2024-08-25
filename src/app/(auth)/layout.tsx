b8052ffe40730294fbeea2ec5dac8dd2373a05ec
Husnain Ali <128280622+husnainalyy@users.noreply.github.com>
2024-08-26 02:44:10 +0500

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,6 +1,6 @@
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="min-h-screen py-10 w-full flex justify-center items-center bg-primary-50 dark:bg-zinc-800 bg-dotted-pattern bg-cover bg-fixed bg-center">
+        <div className="min-h-screen py-10 w-full flex flex-col justify-center items-center text-black dark:text-white bg-primary-50 dark:bg-zinc-800 bg-dotted-pattern bg-cover bg-fixed bg-center">
             <h1>SignUp with the university email only</h1>
             {children}
         </div>
